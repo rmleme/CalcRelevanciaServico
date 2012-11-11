d37e0bd0b03e811bca60d596874b3fd9f60ee379
Rodrigo Mendes Leme <rmleme@gmail.com>
2012-11-11 00:58:01 -0200

    Update src/br/ipt/servico/relevancia/selecao/DialogoURLProcessoNegocio.java

--- src/br/ipt/servico/relevancia/selecao/DialogoURLProcessoNegocio.java
+++ src/br/ipt/servico/relevancia/selecao/DialogoURLProcessoNegocio.java
@@ -1,104 +1,149 @@
 package br.ipt.servico.relevancia.selecao;
 
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 /**
  * Dialogo para inclusao da URL de um processo de negocio.
- *
+ * 
  * @author Rodrigo Mendes Leme
  */
-public class DialogoURLProcessoNegocio extends javax.swing.JDialog {
+public class DialogoURLProcessoNegocio extends JDialog {
+
+    private static final long serialVersionUID = 6198671811220023045L;
 
     private String urlProcessoNegocio;
 
-    public DialogoURLProcessoNegocio(java.awt.Frame parent, String urlProcessoNegocio) {
-        super(parent, true);
-        initComponents();
-        this.urlProcessoNegocio = urlProcessoNegocio;
-        this.txtURLProcessoNegocio.setText(urlProcessoNegocio == null ? "" : urlProcessoNegocio);
+    private JButton btnOK;
+    private JLabel lblURLProcessoNegocio;
+    private JTextField txtURLProcessoNegocio;
+
+    public DialogoURLProcessoNegocio(Frame parent, String urlProcessoNegocio) {
+	super(parent, true);
+	initComponents();
+	this.urlProcessoNegocio = urlProcessoNegocio;
+	this.txtURLProcessoNegocio.setText(urlProcessoNegocio == null ? ""
+		: urlProcessoNegocio);
     }
 
     public String getURLProcessoNegocio() {
-        return this.urlProcessoNegocio;
+	return this.urlProcessoNegocio;
     }
 
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+	lblURLProcessoNegocio = new JLabel();
+	btnOK = new JButton();
+	txtURLProcessoNegocio = new JTextField();
 
-        lblURLProcessoNegocio = new javax.swing.JLabel();
-        btnOK = new javax.swing.JButton();
-        txtURLProcessoNegocio = new javax.swing.JTextField();
+	setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	setTitle("URL do Processo de NegÃ³cio");
+	setModal(true);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("URL do Processo de Negócio");
-        setModal(true);
+	lblURLProcessoNegocio.setText("URL:");
 
-        lblURLProcessoNegocio.setText("URL:");
+	btnOK.setText("OK");
+	btnOK.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent evt) {
+		btnOKActionPerformed(evt);
+	    }
+	});
 
-        btnOK.setText("OK");
-        btnOK.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnOKActionPerformed(evt);
-            }
-        });
+	txtURLProcessoNegocio.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent evt) {
+		txtURLProcessoNegocioActionPerformed(evt);
+	    }
+	});
 
-        txtURLProcessoNegocio.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtURLProcessoNegocioActionPerformed(evt);
-            }
-        });
+	GroupLayout layout = new GroupLayout(getContentPane());
+	getContentPane().setLayout(layout);
+	layout
+		.setHorizontalGroup(layout
+			.createParallelGroup(Alignment.LEADING)
+			.addGroup(
+				layout
+					.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(
+						layout
+							.createParallelGroup(
+								Alignment.LEADING)
+							.addGroup(
+								layout
+									.createSequentialGroup()
+									.addComponent(
+										lblURLProcessoNegocio)
+									.addPreferredGap(
+										ComponentPlacement.RELATED)
+									.addComponent(
+										txtURLProcessoNegocio))
+							.addGroup(
+								Alignment.TRAILING,
+								layout
+									.createSequentialGroup()
+									.addGap(
+										0,
+										350,
+										Short.MAX_VALUE)
+									.addComponent(
+										btnOK,
+										GroupLayout.PREFERRED_SIZE,
+										59,
+										GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap()));
+	layout
+		.setVerticalGroup(layout
+			.createParallelGroup(Alignment.LEADING)
+			.addGroup(
+				layout
+					.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(
+						layout
+							.createParallelGroup(
+								Alignment.BASELINE)
+							.addComponent(
+								lblURLProcessoNegocio)
+							.addComponent(
+								txtURLProcessoNegocio,
+								GroupLayout.PREFERRED_SIZE,
+								GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(
+						ComponentPlacement.RELATED,
+						GroupLayout.DEFAULT_SIZE,
+						Short.MAX_VALUE).addComponent(
+						btnOK).addContainerGap()));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblURLProcessoNegocio)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtURLProcessoNegocio))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 350, Short.MAX_VALUE)
-                        .addComponent(btnOK, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblURLProcessoNegocio)
-                    .addComponent(txtURLProcessoNegocio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnOK)
-                .addContainerGap())
-        );
+	pack();
+    }
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+    private void btnOKActionPerformed(ActionEvent evt) {
+	if (this.txtURLProcessoNegocio.getText() == null
+		|| this.txtURLProcessoNegocio.getText().trim().equals("")) {
+	    JOptionPane
+		    .showMessageDialog(
+			    this,
+			    "URL do processo de neg\u00F3cio \u00E9 um campo obrigat\u00F3rio.",
+			    "Erro", JOptionPane.ERROR_MESSAGE);
+	    this.txtURLProcessoNegocio.requestFocusInWindow();
+	} else {
+	    this.urlProcessoNegocio = this.txtURLProcessoNegocio.getText();
+	    this.dispose();
+	}
+    }
 
-    private void btnOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOKActionPerformed
-        if (this.txtURLProcessoNegocio.getText() == null || this.txtURLProcessoNegocio.getText().trim().equals("")) {
-            JOptionPane.showMessageDialog(
-                    this,
-                    "URL do processo de neg\u00F3cio \u00E9 um campo obrigat\u00F3rio.",
-                    "Erro", JOptionPane.ERROR_MESSAGE);
-            this.txtURLProcessoNegocio.requestFocusInWindow();
-        } else {
-            this.urlProcessoNegocio = this.txtURLProcessoNegocio.getText();
-            this.dispose();
-        }
-    }//GEN-LAST:event_btnOKActionPerformed
-
-    private void txtURLProcessoNegocioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtURLProcessoNegocioActionPerformed
-        this.btnOKActionPerformed(evt);
-    }//GEN-LAST:event_txtURLProcessoNegocioActionPerformed
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnOK;
-    private javax.swing.JLabel lblURLProcessoNegocio;
-    private javax.swing.JTextField txtURLProcessoNegocio;
-    // End of variables declaration//GEN-END:variables
+    private void txtURLProcessoNegocioActionPerformed(ActionEvent evt) {
+	this.btnOKActionPerformed(evt);
+    }
 }
